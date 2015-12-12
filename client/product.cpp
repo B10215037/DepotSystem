5c714e9f95196e78f74dea58e1976f782a46a6da
b10215029 <b10215029@mail.ntust.edu.tw>
2015-12-12 21:13:52 +0800

    add product.cpp content

--- client/product.cpp
+++ client/product.cpp
@@ -2,6 +2,37 @@
 
 Product::Product()
 {
-
+	name = "";
+	stock = 0;
+	price = 0;
 }
 
+QString getName()
+{
+	return name;
+}
+
+void Product::setName(QString name)
+{
+	this->name = name;
+}
+
+int Product::getStock()
+{
+	return stock;
+}
+
+void Product::setStock(int amount)
+{
+	this->stock = amount;
+}
+
+int Product::getPrice()
+{
+	return price;
+}
+
+void Product::setPrice(int price)
+{
+	this.price = price;
+}
